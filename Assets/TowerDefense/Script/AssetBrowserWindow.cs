b875f0b318ec29310913e2780ee9397d214e0cca
gg17117144 <gg17117144@gmail.com>
2024-05-03 01:10:08 +0800

    chore: 美化設定windows視窗

--- Assets/TowerDefense/Script/AssetBrowserWindow.cs
+++ Assets/TowerDefense/Script/AssetBrowserWindow.cs
@@ -1,111 +1,103 @@
-using UnityEngine;
-using UnityEditor;
+#if UNITY_EDITOR
 using System.IO;
+using UnityEditor;
+using UnityEngine;
 
-public class AssetBrowserWindow : EditorWindow
+namespace TowerDefense.Script
 {
-    private string folderPath = "Assets/TowerDefense/Script/ScriptObject"; // 起始資料夾路徑
-    private string[] assetPaths;
-    private bool[] showAssetInfo; // 每個.asset檔案的Toggle狀態
-    private Vector2 scrollPosition; // 捲動位置
-    private string assetRelativePath;
-
-    [MenuItem("Window/資產瀏覽器")] // 將菜單改為繁體中文
-    public static void ShowWindow()
+    public class AssetBrowserWindow : EditorWindow
     {
-        GetWindow<AssetBrowserWindow>("資產瀏覽器"); // 將窗口標題改為繁體中文
-    }
+        private const string FolderPath = "Assets/TowerDefense/Script/ScriptObject"; // 起始資料夾路徑
+        private string[] _assetPaths;
+        private string _assetRelativePath;
 
-    private void OnGUI()
-    {
-        EditorGUILayout.BeginHorizontal();
-        DrawLeftPanel(); // 左邊選單
-        DrawRightPanel(); // 右邊顯示.asset檔案
-        EditorGUILayout.EndHorizontal();
-    }
+        private Vector2 _leftScrollPosition;
+        private Vector2 _rightScrollPosition;
 
-    private void DrawLeftPanel()
-    {
-        EditorGUILayout.BeginVertical(GUILayout.Width(position.width * 0.2f));
-        
-        GUILayout.Label("檔案", EditorStyles.boldLabel); // 將標籤改為繁體中文
-        
-        assetPaths = Directory.GetFiles(folderPath, "*.asset", SearchOption.AllDirectories);
-        
-        foreach (var assetPath in assetPaths)
+        [MenuItem("Window/Data Setting")]
+        public static void ShowWindow()
         {
-            var cleanedFileName = GetCleanedFileName(assetPath);
-            var relativePath = GetRelativePath(assetPath);
-            if (GUILayout.Button($"{cleanedFileName}"))
-            {
-                assetRelativePath = relativePath;
-            }
+            GetWindow<AssetBrowserWindow>("Data Setting");
         }
-        
-        EditorGUILayout.EndVertical();
-    }
-    
-    private void DrawRightPanel()
-    {
-        EditorGUILayout.BeginVertical(GUILayout.Width(position.width * 0.8f));
 
-        // GUILayout.Label("匹配的資產:", EditorStyles.boldLabel); // 將標籤改為繁體中文
-        //
-        // // 初始化 Toggle 狀態數組
-        // if (showAssetInfo == null || showAssetInfo.Length != assetPaths.Length)
-        // {
-        //     showAssetInfo = new bool[assetPaths.Length];
-        // }
-        //
-        // scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
-        //
-        // for (int i = 0; i < assetPaths.Length; i++)
-        // {
-        //     string assetRelativePath = GetRelativePath(assetPaths[i]);
-        //     showAssetInfo[i] = EditorGUILayout.ToggleLeft(assetRelativePath, showAssetInfo[i]);
-        //
-        //     if (showAssetInfo[i])
-        //     {
-        //         DisplayAssetInfo(assetRelativePath);
-        //         GUILayout.Space(10);
-        //     }
-        // }
-        
-        DisplayAssetInfo(assetRelativePath);
-        // DisplayAssetInfo("Assets/TowerDefense/Script/ScriptObject/EnemySo/Pirate_02.asset");
-
-        EditorGUILayout.EndScrollView();
-
-        EditorGUILayout.EndVertical();
-    }
-
-    private void DisplayAssetInfo(string assetRelativePath)
-    {
-        Object asset = AssetDatabase.LoadAssetAtPath<Object>(assetRelativePath);
-        if (asset != null)
+        private void OnGUI()
         {
-            SerializedObject serializedObject = new SerializedObject(asset);
-            SerializedProperty property = serializedObject.GetIterator();
+            EditorGUILayout.BeginHorizontal();
 
-            while (property.NextVisible(true))
+            // 左側面板
+            EditorGUILayout.BeginVertical(GUI.skin.box, GUILayout.Width(position.width * 0.3f));
+            DrawLeftPanel();
+            EditorGUILayout.EndVertical();
+
+            // 右側面板
+            EditorGUILayout.BeginVertical(GUI.skin.box, GUILayout.Width(position.width * 0.7f));
+            _rightScrollPosition = EditorGUILayout.BeginScrollView(_rightScrollPosition);
+            DrawRightPanel();
+            EditorGUILayout.EndScrollView();
+            EditorGUILayout.EndVertical();
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void DrawLeftPanel()
+        {
+            EditorGUILayout.LabelField("檔案", EditorStyles.boldLabel);
+
+            _leftScrollPosition = EditorGUILayout.BeginScrollView(_leftScrollPosition);
+
+            _assetPaths = Directory.GetFiles(FolderPath, "*.asset", SearchOption.AllDirectories);
+
+            foreach (var assetPath in _assetPaths)
             {
-                EditorGUILayout.PropertyField(property, true);
+                var cleanedFileName = GetCleanedFileName(assetPath);
+                var relativePath = GetRelativePath(assetPath);
+                if (GUILayout.Button($"{cleanedFileName}"))
+                {
+                    _assetRelativePath = relativePath;
+                }
             }
 
-            serializedObject.ApplyModifiedProperties();
+            EditorGUILayout.EndScrollView();
         }
-    }
 
-    private string GetRelativePath(string fullPath)
-    {
-        return fullPath.Replace(Application.dataPath, "Assets");
-    }
-    
-    //做字串處理
-    static string GetCleanedFileName(string filePath)
-    {
-        string fileName = Path.GetFileNameWithoutExtension(filePath);
-        string[] parts = fileName.Split('/'); // 假設檔名可能包含 '/' 字元
-        return parts[parts.Length - 1]; // 返回分割後的最後一個部分
+        private void DrawRightPanel()
+        {
+            EditorGUILayout.LabelField("匹配的資產:", EditorStyles.boldLabel);
+
+            DisplayAssetInfo(_assetRelativePath);
+        }
+
+        private void DisplayAssetInfo(string assetRelativePath)
+        {
+            if (string.IsNullOrEmpty(assetRelativePath))
+                return;
+
+            Object asset = AssetDatabase.LoadAssetAtPath<Object>(assetRelativePath);
+            if (asset != null)
+            {
+                SerializedObject serializedObject = new SerializedObject(asset);
+                SerializedProperty property = serializedObject.GetIterator();
+
+                while (property.NextVisible(true))
+                {
+                    EditorGUILayout.PropertyField(property, true);
+                }
+
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+
+        private string GetRelativePath(string fullPath)
+        {
+            return fullPath.Replace(Application.dataPath, "Assets");
+        }
+
+        static string GetCleanedFileName(string filePath)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(filePath);
+            string[] parts = fileName.Split('/');
+            return parts[^1];
+        }
     }
 }
+#endif
