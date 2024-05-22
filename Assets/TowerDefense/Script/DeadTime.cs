4cfef6dfdde14a651b4967a4d229f59d3b7099e4
gg17117144 <gg17117144@gmail.com>
2024-05-22 23:59:28 +0800

    feat: 修復死亡物件的腳本

---
+++ Assets/TowerDefense/Script/DeadTime.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace TowerDefense.Script
+{
+    public class DeadTime : MonoBehaviour
+    {
+        void Start()
+        {
+            Invoke(nameof(Destroy), 1f);
+        }
+
+        private void Destroy()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
