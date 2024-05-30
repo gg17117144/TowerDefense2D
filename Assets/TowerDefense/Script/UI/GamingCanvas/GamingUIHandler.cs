7e80edbcf7b67edbada4b7108d5990691c311518
gg17117144 <gg17117144@gmail.com>
2024-05-31 03:18:39 +0800

    feat: 新增遊戲UI整體控制

--- Assets/TowerDefense/Script/UI/GamingCanvas/GamingUIHandler.cs
+++ Assets/TowerDefense/Script/UI/GamingCanvas/GamingUIHandler.cs
@@ -1,12 +1,18 @@
 using System;
+using System.Collections.Generic;
+using DG.Tweening;
 using NaughtyAttributes;
+using TMPro;
 using TowerDefense.Script.UI.GamingCanvas;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace TowerDefense.Script.UI
 {
     public class GamingUIHandler : MonoBehaviour
     {
+        public static GamingUIHandler Instance;
+        
         public HpUIController HpUIController => hpUIController;
         [SerializeField] [Required] private HpUIController hpUIController;
 
@@ -18,6 +24,55 @@ namespace TowerDefense.Script.UI
 
         public ExperienceUIController ExperienceUIController => experienceUIController;
         [SerializeField] [Required] private ExperienceUIController experienceUIController;
+
+        private Image[] _imageList;
+        private TextMeshProUGUI[] _textList;
         
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start()
+        {
+            _imageList = GetComponentsInChildren<Image>();
+            _textList = GetComponentsInChildren<TextMeshProUGUI>();
+        }
+
+        [Button]
+        public void OpenGamingUI()
+        {
+            foreach (Image image in _imageList)
+            {
+                image.DOFade(1, 1f);
+            }
+            foreach (TextMeshProUGUI text in _textList)
+            {
+                Debug.Log("打開text");
+                text.gameObject.SetActive(true);
+            }
+        }
+        
+        [Button]
+        public void CloseGamingUI()
+        {
+            foreach (Image image in _imageList)
+            {
+                image.DOFade(0, 1f);
+            }
+            foreach (TextMeshProUGUI text in _textList)
+            {
+                Debug.Log("關閉text");
+                text.gameObject.SetActive(false);
+            }
+        }
     }
 }
