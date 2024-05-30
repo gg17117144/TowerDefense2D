98be18655e78d253645b1b263d13e97844262a59
gg17117144 <gg17117144@gmail.com>
2024-05-31 03:24:25 +0800

    feat: 新增GameManager狀態機 (沒做完

--- Assets/TowerDefense/Script/GameManager.cs
+++ Assets/TowerDefense/Script/GameManager.cs
@@ -1,15 +1,50 @@
 using System;
 using UnityEngine;
+using UnityHFSM;
 
-namespace TowerDefense.Script
+namespace TowerDefense.Prefab.Script
 {
     public class GameManager : MonoBehaviour
     {
-        public static GameManager instance;
+        private StateMachine _fsm;
+        //TODO 狀態機去觸發事件
+        public static GameManager Instance;
+        
         private void Awake()
         {
-            instance = this;
+            Instance = this;
             DontDestroyOnLoad(gameObject);
         }
+
+        private void Initialize()
+        {
+            _fsm = new StateMachine();
+            _fsm.AddState("Defense", onLogic: state => Defense());
+            _fsm.AddState("LevelUp", onLogic: state => LevelUp());
+            _fsm.AddState("Dead", onLogic: state => Dead());
+            _fsm.SetStartState("Defense");
+            _fsm.Init();
+        }
+        
+        private void Start()
+        {
+            Initialize();
+        }
+
+        private void Defense()
+        {
+            
+        }
+        
+        private void Dead()
+        {
+
+        }
+
+        private void LevelUp()
+        {
+
+        }
+
     }
 }
