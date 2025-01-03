40d05c39bbba0117a1c03e0b2bc3345b51288c95
gg17117144 <gg17117144@gmail.com>
2025-01-03 20:18:26 +0800

    feat: 移除不關GameManager控制的腳本內容

--- Assets/TowerDefense/Script/GameManager.cs
+++ Assets/TowerDefense/Script/GameManager.cs
@@ -1,47 +1,8 @@
 using TowerDefense.Script.Utilities;
-using UnityEngine;
-using UnityHFSM;
 
 namespace TowerDefense.Script
 {
     public class GameManager : Singleton<GameManager>
     {
-        private StateMachine _fsm;
-
-        //TODO 狀態機去觸發事件
-        public static GameManager instance;
-
-        private void Awake()
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-
-        private void Initialize()
-        {
-            _fsm = new StateMachine();
-            _fsm.AddState("Defense", onLogic: state => Defense());
-            _fsm.AddState("LevelUp", onLogic: state => LevelUp());
-            _fsm.AddState("Dead", onLogic: state => Dead());
-            _fsm.SetStartState("Defense");
-            _fsm.Init();
-        }
-
-        private void Start()
-        {
-            Initialize();
-        }
-
-        private void Defense()
-        {
-        }
-
-        private void Dead()
-        {
-        }
-
-        private void LevelUp()
-        {
-        }
     }
 }
